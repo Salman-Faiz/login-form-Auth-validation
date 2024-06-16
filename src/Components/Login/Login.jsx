b8db32d37ef5909a4627c59a1e3022adb182f189
Salman-Faiz <salmanfaiz.cse@gmail.com>
2024-06-16 10:49:30 +0600

    login and register from added

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div>
-           <form >
-            <input type="email" value={'email'} name="email" />
+       <div className="container mx-auto">
+         <div className="border mt-40 bg-green-200 rounded-lg text-center">
+            <h3 className="pt-8  text-3xl font-semibold">Please login</h3>
+           <form  className="py-10">
+           
+            <input className="border text-2xl px-4 py-2 rounded-md mb-5" type="email" name="email" id="" placeholder="Email" /> <br />
+            <input className="border text-2xl px-4 py-2 rounded-md" type="password" name="password" id="" placeholder="password" /> <br />
+            <p className="">forget password??</p>
+            <input className="border btn btn-primary text-2xl" type="submit" value="submit" />
            </form>
+           <p className='py-4 text-red-600'>New Here?? Please create an Account first  <Link className='font-bold text-black text-xl' to={'/register'}>Register Now</Link></p>
         </div>
+       </div>
     );
 };
 
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-                <h3>this is Register</h3>
+    
+      <div className="container mx-auto">
+          <div className="border mt-40 bg-blue-200 rounded-lg text-center">
+              <h3 className="pt-8  text-3xl font-semibold">Please Register</h3>
+           <form  className="py-10 space-y-5">
+           <input className="border text-2xl px-4 py-2 rounded-md" type="text" name='name' placeholder='Name' /><br />
+            <input className="border text-2xl px-4 py-2 rounded-md" type="email" name="email" id="" placeholder="Email" /> <br />
+            <input className="border text-2xl px-4 py-2 rounded-md" type="password" name="password" id="" placeholder="password" /> <br />
+            <input className="border btn btn-primary text-2xl" type="submit" value="submit" />
+           </form>
+           <p className='py-4 text-red-600'>Already have an account?? Please go to <Link className='font-bold text-black text-xl' to={'/login'}>Login</Link></p>
         </div>
+
+      </div>
     );
 };
 
